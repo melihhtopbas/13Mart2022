bcf3b3098399deebf60cf5a34a51b91ed2efbcae
topba <topba@MELIH>
2022-03-14 00:38:22 +0300

    Proje dosyası ekle.

---
+++ 12Mart2022/Controllers/PersonelController.cs
@@ -0,0 +1,66 @@
+﻿using _12Mart2022.Models.EntityFramework;
+using _12Mart2022.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _12Mart2022.Controllers
+{
+    public class PersonelController : Controller
+    {
+        MyNewWorkEntities db = new MyNewWorkEntities();
+        // GET: Personel
+        public ActionResult Index()
+        {
+            var model = db.Personel.ToList();
+            return View(model);
+        }
+
+        public ActionResult Yeni()
+        {
+            var model = new PersonelFormViewModel() {
+                Departmanlar = db.Departman.ToList()
+            
+            };
+            return View("PersonelForm",model);
+        }
+        public ActionResult Kaydet(Personel personel)
+        {
+            if (personel.Id==0)//ekleme
+            {
+                db.Personel.Add(personel);
+            }
+            else//Güncelleme
+            {
+                db.Entry(personel).State = System.Data.Entity.EntityState.Modified;
+            }
+            
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Guncelle(int tasinacakId)
+        {
+            var model = new PersonelFormViewModel()
+            {
+               Departmanlar = db.Departman.ToList(),
+            Personel = db.Personel.Find(tasinacakId)
+            };
+            return View("PersonelForm", model);
+        }
+        public ActionResult Sil(int tasinacakId)
+        {
+            var silinecekPersonel = db.Personel.Find(tasinacakId);
+            if (silinecekPersonel==null)
+            {
+                return HttpNotFound();
+            }
+            db.Personel.Remove(silinecekPersonel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ 12Mart2022/ViewModels/PersonelFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using _12Mart2022.Models.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _12Mart2022.ViewModels
+{
+    public class PersonelFormViewModel
+    {
+
+        public IEnumerable<Departman> Departmanlar;
+        public Personel Personel { get; set; }
+    }
+}
